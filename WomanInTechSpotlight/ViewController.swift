314f1dfa9873e56148e2f78012af7fa9f75fe634
Vivian Phung <VPhung11@gmail.com>
2018-06-18 15:59:51 -0700

    first commit

---
+++ WomanInTechSpotlight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WomanInTechSpotlight
+//
+//  Created by Vivian Phung on 6/18/18.
+//  Copyright © 2018 Vivian Phung. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
